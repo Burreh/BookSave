94049cbdf89aed08f10c2432672a23e7389b8fe0
Boorhan Heele <81237771+Burreh@users.noreply.github.com>
2022-05-18 15:20:02 +0200

    Fetch data from NYT-api

--- booksave/src/App.js
+++ booksave/src/App.js
@@ -7,11 +7,13 @@ export default function App() {
   useEffect(() => {
     // TEST
     //fetch('http://www.omdbapi.com/?i=tt3896198&apikey=96da8d24')
-    fetch('https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAJ')
+    // GDBdGMGvhAIUBM38hfoifg3V5MIB3h8H
+    fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=GDBdGMGvhAIUBM38hfoifg3V5MIB3h8H')
       .then(res => res.json())
       .then(data => setData(data))
 
   }, [])
+
   return (
     <div className="container">
       <h1>BookSave</h1>
