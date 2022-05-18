0819be7a82d90620de457f39b67d91dae0b63cb5
Boorhan Heele <81237771+Burreh@users.noreply.github.com>
2022-05-18 17:06:37 +0200

    Added style to saveButton

--- booksave/src/components/SaveButton.js
+++ booksave/src/components/SaveButton.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function SaveButton() {
     return (
-        <div>SaveButton</div>
+        <button type="button" className="btn btn-primary">Primary</button>
     )
 }
