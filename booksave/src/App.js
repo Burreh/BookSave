a8d90811f95587349a209f13d11ebdafe148cd64
Boorhan Heele <81237771+Burreh@users.noreply.github.com>
2022-05-18 14:08:15 +0200

    Fetching API
    First test of fetching API

--- booksave/src/App.js
+++ booksave/src/App.js
@@ -1,11 +1,21 @@
+import React, { useState, useEffect } from 'react'
 import './style/App.css';
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // TEST
+    //fetch('http://www.omdbapi.com/?i=tt3896198&apikey=96da8d24')
+    fetch('https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAJ')
+      .then(res => res.json())
+      .then(data => setData(data))
+
+  }, [])
   return (
     <div className="container">
-
+      <h1>BookSave</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
-
-export default App;
